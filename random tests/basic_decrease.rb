56315ba9f55affdeaf90796957046a92b8e0de63
Allan <4llankayan@gmail.com>
2021-04-24 10:55:14 -0300

    Delete random tests directory

--- random tests/basic_decrease.rb
+++
@@ -1,5 +0,0 @@
-array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-
-for i in (array.length - 1).downto(0)
-    puts array[i]
-end
--- random tests/sort_array.rb
+++
@@ -1,5 +0,0 @@
-array = [8, 6, 7, 2, 5, 1, 3, 0, 9, 4]
-
-
-puts "Array desorganizada: #{array}"
-puts "Array organizada: #{array.sort}" 
